045260be9d4fc01dfed29a7eac298d7816b143fc
brunEdo <edobruno.m@gmail.com>
2021-09-27 16:18:51 +0200

    10 emails list

--- js/app.js
+++ js/app.js
@@ -8,10 +8,24 @@ const app = new Vue (
     {
         el: "#root",
         data: {
-            
+            emailList: []
         },
         methods: {
-            
+
+        },
+        mounted() {
+            for(let i = 0; i < 10; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then((response) => {
+                        console.log("oi");
+                        // handle success
+                        this.emailList.push(response.data.response);
+                    })
+                    .catch((error) => {
+                        // handle error
+                        console.log(error);
+                    })
+            }
         }
     }
 )
